91e3ff6ce2f4a83b7b1449df0df4658cc5faaebb
Marina Jolić <marinajolic@MacBook-Pro-2.local>
2024-10-10 23:59:41 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`Trenutni broj: ${count}`);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Povećaj</button>
     </div>
   );
-}
-
-export default App;
+};
